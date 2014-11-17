062c538c9ac6f58717f18df20097c8afdc80c992
hclark01 <hcc3@optonline.net>
2014-11-17 09:49:17 -0500

    output module written, uncompiled, untested

--- output.erl
+++ output.erl
@@ -8,6 +8,54 @@ start(ProcName) -> register(ProcName,
 remove(Proc, Type) -> Proc ! {remove, Type}.
 add(Proc, Type) -> Proc ! {add, Type}.
 endSimulation(Proc) -> Proc ! {end}.
+newStationStat(Proc, Stats) -> %Stats: {StationName, NumPass, HasIn, HasOut}
+    Proc ! {stationStat, Stats}.
+newTrainStat(Proc, Stats) -> %Stats: {CurrLoc, NextOrCurrStation, NumPass}
+                                % CurrLoc should be atom station or track
+    Proc ! {trainStat, Stats}.
 
-loop(TrainCount, StationCount) ->
-    
+printTrains([], _) -> {ok};
+printTrains([{CurrLoc, Station, Pass}|Trains], Device) ->
+    case CurrLoc of
+        station -> io:fwrite(Device, "train Station:~p Passengers:~p~n",
+                                [Station, Pass]);
+        track -> io:fwrite(Device, "train Approaching:~p Passengers:~p~n",
+                                [Station, Pass])
+    end,
+    printTrains(Trains, Device).
+
+printStations([], _) -> {ok};
+printStations([{StationName, NumPass, HasIn, HasOut}|Stations], Device) ->
+    io:fwrite(Device, "station Name:~p Passengers:~p InTrain:~p OutTrain:~p~n",
+                    [StationName, NumPass, HasIn, HasOut]),
+    printStations(Stations, Device).
+
+loop(TrainCnt, StationCnt, TrainCnt, StationCnt, TrainStats, StationStats,
+irintStations([], _) -> {ok}.
+    Device) when length(TrainStats) =:= TrainCnt and 
+                length(StationStats =:= StationCnt ->
+    printTrains(TrainStats),
+    printStations(StationStats),
+    loop(TrainCnt, StationCnt, [], [], Device);
+loop(TrainCnt, StationCnt, TrainStats, StationStats, Device) ->
+    receive
+        {add, train} -> loop(TrainCnt + 1, StationCnt, TrainStats, StationStats,
+                                Device);
+        {add, station} -> loop(TrainCnt, StationCnt + 1, TrainStats,
+                                StationStats, Device);
+        {remove, train} -> loop(TrainCnt - 1, StationCnt, TrainStats, 
+                                StationStats, Device);
+        {remove, station} -> loop(TrainCnt, StationCnt - 1, TrainStats, 
+                                StationStats, Device);
+        {clockTick, Minute} -> io:fwrite(Device, "Minute ~p~n", [Minute]),
+                                loop(TrainCnt, StationCnt, TrainStats, 
+                                    StationStats, Device);
+        {trainStat, Stat} -> loop(TrainCnt, StationCnt, [Stat|TrainStats],
+                                StationStats, Device);
+        {stationStat, Stat} -> loop(TrainCnt, StationCnt, TrainStats,
+                                [Stat|StationStats, Device);
+
+        {endSim} -> printTrains(TrainStats),
+                    printStations(StationStats),
+                    {ok}
+    end.
