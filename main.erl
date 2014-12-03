11683e7e67b117613838c47387d4975899f2b6c9
hannahcclark <hannah_clark@mac.com>
2014-12-02 19:55:19 -0500

    single delay working, I think, did find issue in train leaving station maybe too soon when it starts at minute 1

--- main.erl
+++ main.erl
@@ -5,6 +5,8 @@
 % Original Author: Hannah Clark
 % Date: 11/8/14
 % ChangeLog:
+%    12/02/14 - HCC - fixed delay to not send minute done when about to
+%                     remove from clock
 %    11/24/14 - HCC - fixed bugs in code to exit simulation
 %    11/23/14 - HCC - fixed bugs in delay loop function and procsAlive function
 %    11/22/14 - HCC - fixed bugs in start due to order of function calls that appeared in integration testing
@@ -104,7 +106,10 @@ delayLoop(Delays) ->
                 end
             end, [], Delays),
             io:fwrite("delay loop minute done~n", []),
-            clk ! {minuteDone},
+            if
+                Remaining =:= [] -> io:fwrite("Done~n", []), ok;
+                true -> clk ! {minuteDone}
+            end,
             delayLoop(Remaining)
     end.
 
